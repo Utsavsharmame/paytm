9beb5de38e5d008e208e3051fe29333b17eadb3d
Utsav <sutsav237@gmail.com>
2024-12-09 23:29:16 +0530

    install the npm

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+	JWT_SECRET: "your-jwt-secret"
+}
+
+const JWT_SECRET = "utsavSecret";
+module.exports= JWT_SECRET_SECRET
---
+++ backend/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors =  require("cors");
+
+app.use(cors());
+
+// body parser
+app.use(express.json());
+
+const mainRouter = require("./routes/index");
+
+const app = express();
+
+app.use("/api/v1", mainRouter);
+
+app.listen(3000);
+
+// /api/v1/user/signup
+// /api/v1/user/signin
+// /api/v1/user/changePassword ....
+
+// /api/v1/account/transferMoney
+// /api/v1/account/balance
+
+
+
+
