97168c97310960903db19cdf26f92b00b4a2f969
Utsav <sutsav237@gmail.com>
2024-12-11 21:20:54 +0530

    Delete backend directory

--- backend/config.js
+++
@@ -1,7 +0,0 @@
-
-module.exports = {
-	JWT_SECRET: "your-jwt-secret"
-}
-
-const JWT_SECRET = "utsavSecret";
-module.exports= JWT_SECRET_SECRET
--- backend/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = require("./user");
-
-const router = express.Router();
-
-router.use("/user", userRouter);
-
-module.exports = router;
-// api/v1
